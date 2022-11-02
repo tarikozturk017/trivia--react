f163af58130c6590cfcdffa1708405edf74a7824
tarikozturk017 <75666245+tarikozturk017@users.noreply.github.com>
2022-11-01 21:16:45 -0400

    homepage created

--- trivia/src/App.js
+++ trivia/src/App.js
@@ -1,10 +1,23 @@
 import React from "react"
+import Homepage from "./components/Homepage";
 import './style.css'
 
 function App() {
+
+  // const [questions, setQuestions] = React.useState([])
+  // React.useEffect(() => {
+  //   fetch("https://opentdb.com/api.php?amount=10")
+  //     .then(res => res.json())
+  //     .then(data => setQuestions(data.results))
+  // }, [])
+
+
   return (
     <div className="App">
-     
+     <img className="img--bottom-left" src="blob 5.png" alt=""/>
+     {/* <p>{questions[0] && questions[0].category}</p> */}
+     <Homepage />
+     <img className="img--upper-right" src="blob 15.png" alt=""/>
     </div>
   );
 }
---
+++ trivia/src/components/Homepage.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Homepage() {
+    return (
+        <div className="homepage">
+            <h2>Quizzical</h2>
+            <p>Some description if needed</p>
+            <button>Start quiz</button>
+        </div>
+    )
+}
